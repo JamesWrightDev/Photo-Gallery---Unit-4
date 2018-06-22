381e7868785011d7403056e1ce56c92e2050dda7
James <jameswrightfilm@gmail.com>
2018-06-22 11:08:39 +0100

    Add lightbox

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $("#myInput").on("keyup", function() {
+  $('#myInput').on("keyup", function() {
     var value = $(this).val().toLowerCase();
     $("figure").filter(function() {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
